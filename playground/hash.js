e87ff364d0e15f67b4acf03ab5eab463e8a3a78d
Jeet <jeet025@hotmail.com>
2017-12-02 14:14:29 +0530

    added password hashing

--- playground/hash.js
+++ playground/hash.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const  jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id :24
-};
 
-var token = jwt.sign(data , 'asddgggfe');
-console.log(token);
+var password = 'sagarbanchod';
 
-var decoded = jwt.verify(token, 'asddgggfe' );
-console.log(decoded);
+// bcrypt.genSalt(10 , (err, salt)=>{
+//   bcrypt.hash(password, salt ,(err, hash) =>{
+//     console.log(hash);
+//   });
+// })
+
+var hashedpw = '$2a$10$lZ6MatV0Kas8BY1kTHuUGuiOeuiGBgLpBuXRqCBUQMeCJ7lH9Oabq';
+
+bcrypt.compare(password, hashedpw, (err, result)=>{
+  console.log(result);
+})
+
+
+// var data = {
+//   id :24
+// };
+
+// var token = jwt.sign(data , 'asddgggfe');
+// console.log(token);
+
+// var decoded = jwt.verify(token, 'asddgggfe' );
+// console.log(decoded);
 // var msg = 'My name is Jeet Adhikary';
 // var hash = SHA256(msg).toString();
 //
